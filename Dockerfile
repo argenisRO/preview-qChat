f280a9c08a107410ca440fb090608d53d6b0117c
Argenis Rodriguez <argy16@gmail.com>
2018-10-07 12:57:46 -0400

    changes to local settings for testing

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.6
 ENV PYTHONUNBUFFERED 1
 ENV PRODUCTION "False"
+ENV DB_HOST "db"
+ENV REDIS_HOST "redis"
 WORKDIR /usr/src/app
 ADD requirements.txt /usr/src/app
 RUN pip install -r requirements.txt
--- qChat/settings/local.py
+++ qChat/settings/local.py
@@ -4,7 +4,7 @@ PROJECT_DIR = path.abspath(path.join(path.dirname( __file__ ), '..', 'chat'))
 
 SECRET_KEY = 'j5cr7_3=7z*&6he4m3z@o&11^k2&6%3(37uso9q+-1)87^q1yk'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -13,7 +13,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': environ.get('DB_HOST', 'localhost'),
         'PORT': 5432,
     }
 }
@@ -22,15 +22,15 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": (environ.get('REDIS_HOST', 'localhost'), 6379),
+            "hosts": [(environ.get('REDIS_HOST', 'localhost'), 6379)]
         },
     },
 }
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    path.join(PROJECT_DIR, "static"),
+    path.join(PROJECT_DIR, "static/"),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = path.join(BASE_DIR, "media_dev")
+MEDIA_ROOT = path.join(BASE_DIR, "media_dev/")
