65c1fe014f11e0d3901a42411583904db8858de1
Argenis Rodriguez <argy16@gmail.com>
2018-10-08 17:48:34 -0400

    more modifications to local test setting

--- qChat/settings/local.py
+++ qChat/settings/local.py
@@ -1,4 +1,5 @@
 from .base import *
+from sys import argv
 
 PROJECT_DIR = path.abspath(path.join(path.dirname( __file__ ), '..', 'chat'))
 
@@ -10,11 +11,16 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        # Separate the Postgresql Database from the Testing Sqlite3 Database
+        'ENGINE': 'django.db.backends.sqlite3' if 'test' in argv or 'test_coverage' in argv else 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': environ.get('DB_PASS', ''),
         'HOST': environ.get('DB_HOST', 'localhost'),
         'PORT': 5432,
+        'TEST': {
+            'NAME': path.join(BASE_DIR, 'db.sqlite3')
+        }
     }
 }
 
