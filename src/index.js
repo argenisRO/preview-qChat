514b9720ff4c003281ea5b6acc9e05f6c8b265d0
Argenis Rodriguez <argy16@gmail.com>
2019-01-19 14:06:55 -0500

    baseline setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { CssBaseline } from '@material-ui/core'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import Root from './components/Root'
+import theme from './theme'
+
 import './Style.css'
 
-ReactDOM.render(<Root />, document.getElementById('main'))
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Root />
+    </MuiThemeProvider>,
+    document.getElementById('main')
+)
