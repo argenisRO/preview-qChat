d3498196d4716e9fa6ed597b97c724431bf6170e
Argenis Rodriguez <argy16@gmail.com>
2019-01-21 01:26:59 -0500

    Changed Styling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { CssBaseline } from '@material-ui/core'
-import { MuiThemeProvider } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
 import Root from './components/Root'
 import theme from './theme'
-
 import './Style.css'
 
 ReactDOM.render(
-    <MuiThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
         <CssBaseline />
         <Root />
-    </MuiThemeProvider>,
+    </ThemeProvider>,
     document.getElementById('main')
 )
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,40 @@
-import { createMuiTheme } from '@material-ui/core'
-
-const theme = createMuiTheme({
-    //
-})
+const theme = {
+    background: {
+        color: {
+            main: '#222D32',
+            secondary: '#253646',
+        },
+    },
+    container: {
+        color: {
+            main: '#1F2933',
+            secondary: '',
+        },
+    },
+    spacing: {
+        general: 8,
+        container: 5,
+    },
+    shadow: {
+        soft:
+            '0px 1px 8px 0px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 3px 3px -2px rgba(0,0,0,0.12)',
+    },
+    breakpoint: {
+        values: 0,
+        sm: 600,
+        md: 960,
+        lg: 1280,
+        xl: 1920,
+    },
+    profile: {
+        user: {
+            status: {
+                online: '#A5F134',
+                away: '#F1F11E',
+                busy: '#F13434',
+            },
+        },
+    },
+}
 
 export default theme
